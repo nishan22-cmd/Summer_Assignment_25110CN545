2e8e2134dd64e209ace4a1fa10c14a4aa379245e
nishan22-cmd <nishankumar499@gmail.com>
2026-06-20 14:25:08 +0530

    Added Day 20 solutions

---
+++ Day_20/Q78.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int n, i, j;
+    int flag = 1;
+
+    printf("Enter order of matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            if(a[i][j] != a[j][i])
+            {
+                flag = 0;
+            }
+        }
+    }
+
+    if(flag == 1)
+    {
+        printf("Symmetric Matrix");
+    }
+    else
+    {
+        printf("Not a Symmetric Matrix");
+    }
+
+    return 0;
+}
